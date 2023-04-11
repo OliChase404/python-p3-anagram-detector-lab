17182bdc3309f582227e769587b12a6133c6a977
Oliver <olichase91@gmail.com>
2023-04-10 17:06:54 -0700

    Solved Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = [i for i in word_list if sorted(i) == sorted(self.word)]
+        return anagrams
+
+
